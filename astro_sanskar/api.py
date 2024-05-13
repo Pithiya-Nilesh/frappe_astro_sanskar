d631a0b0024b1bf70bd86aba1f6fa5174ef9c066
Pithiya-Nilesh <pithiyanilesh87@gmail.com>
2024-05-13 17:46:06 +0530

    career page issue

--- astro_sanskar/api.py
+++ astro_sanskar/api.py
@@ -1,12 +1,22 @@
 import frappe
 
-# @frappe.whitelist(allow_guest=True)
-# def check():
-#     frappe.publish_realtime(
-#             event="latest_chat_updates",
-#             message="message",
-#             user="user",
-#         )
+@frappe.whitelist(allow_guest=True)
+def send_form_failed_notification(message):
+    frappe.sendmail(
+        recipients=["nilesh@sanskartechnolab.com"], 
+        sender="sanskartechnolabtest@gmail.com",
+        subject="test",
+        template="send_form_failed_notification_email_template",
+        args={
+            "page_name": "test page",                
+            "host_name": frappe.utils.get_url(),
 
-
-frappe.publish_realtime('demo', data={'key': 'value'})
+            # "first_name": full_name,
+            # "email": email,
+            # "phone": phone,
+            # "message": message,
+            # "enquiry_source": enquiry_source,
+            # "demo_for": demoForValue,
+            # "query_type": queryTypeValue,
+            "message" : message
+        },now=True)
